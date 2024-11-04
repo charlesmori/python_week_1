d651e664a8ba0d89c4cc222e6b9524fe14960dca
Charles Mori <charlesmori09@gmail.com>
2024-11-04 20:08:40 +0200

    my week one assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+
+name = input("Please Enter your Name: ")
+age = input("Enter your Age: ")
+location = input("Enter your Location: ")
+print("Hello! " + name + " you are " + age + " years old and lives in " + location)
